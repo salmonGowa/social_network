e1f45c2d47690bab51170dac4b9ac71be1d9c34f
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-21 14:32:31 +0300

    file storage configuration

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const __storage=multer.diskStorage({
     }
 });
 
-const upload= multer({ storage });
+const upload = multer({storage});
 
 /*mongoose setuo*/
 const PORT= process.env.PORT || 6001;
@@ -42,5 +42,5 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
-    app.listen(PORT, ()=>console.log('server Port: ${PORT}'));
+    app.listen(PORT, ()=>console.log('Server Port: ${PORT}'));
 }).catch((error) => console.log('${error} did not connect'));
