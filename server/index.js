82365232564ba31ab97db77915db5aa0ea8679f2
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-21 16:23:50 +0300

    ///

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import {register} from "./controllers/auth.js";
 
 /*configurations*/
@@ -38,6 +39,9 @@ const storage=multer.diskStorage({
 const upload = multer({storage});
 /*routes with files*/
 app.post("/auth/register", upload.single("picture"), register);
+
+/*routes*/
+app.use("/auth", authRoutes);
 
 /*mongoose setuo*/
 const PORT= process.env.PORT || 6001;
