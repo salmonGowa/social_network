14d37e8224830cbe5993ac603aaf49f33e41ed85
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-21 15:46:52 +0300

    assets

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,43 @@
 import bcrypt from "brypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+/*register user*/
+
+export const register=async(req, res)=>{
+    try{
+        const{
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt=await bcrypt.genSalt();
+        const passwordHash=await bcrypt.has(password, salt);
+
+        const newUser =new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random()*10000),
+            impressions: Math.floor(Math.random()*10000),
+
+        });
+        const saveUser= await newUser.save();
+        res.status(201).json(savedUser);
+
+    } catch (err){
+        res.status(500).json({error:err.message});
+
+    }
+};
