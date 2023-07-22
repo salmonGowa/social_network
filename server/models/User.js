37fdcb8b8f77d16305047d6f135c8cc757221d47
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-22 10:36:21 +0300

    userauthentication

--- server/models/User.js
+++ server/models/User.js
@@ -41,6 +41,6 @@ const UserSchema =new mongoose.Schema(
     {timestamps:true}
 );
 
-const User=mongoose.model("USer", UserSchema)
+const User=mongoose.model("User", UserSchema)
 
 export default User;
