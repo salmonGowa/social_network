62655e2a7dcae990403ba320fdabb2e7c8989c71
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-24 13:01:49 +0300

    saving data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import {createPost} from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import{users,posts} from "./data/index.js";
 /*configurations*/
 
 const __filename=fileURLToPath(import.meta.url);
@@ -55,4 +58,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(()=>{
     app.listen(PORT, ()=>console.log('Server Port: ${PORT}'));
+
+    /*Add data  */
+    User.insertMany(users);
+    Post.insertMany(posts);
 }).catch((error) => console.log('${error} did not connect'));
