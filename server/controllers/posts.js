a7dba7e69630b5d86583a7a78a776eea975a67a6
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-24 13:01:32 +0300

    loading data

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,7 +19,6 @@ export const createPost=async(req,res)=>{
             likes:{},
             comments:[],
             
-
         })
 
         await newPost.save();
@@ -30,6 +29,58 @@ export const createPost=async(req,res)=>{
 
     }catch (err){
         res.status(409).json({message: err.message})
+
+    }
+}
+
+/**read */
+export const getFeedPosts=async (req,res)=>{
+    try{
+         const post= await Post.find();
+          res.status(200).json(post);
+
+    }catch(err){
+        res.status(404).json({message: err.message})
+    }
+}
+
+export const getUserPosts=async (req,res)=>{
+    try{
+        const {userId}=req.params;
+         const post= await Post.find({userId});
+          res.status(200).json(post);
+
+    }catch(err){
+        res.status(404).json({message: err.message})
+    }
+}
+
+/**update */
+
+export const likePost=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const{userId}=req.body;
+        const post=await Post.findById(id);
+        const isLiked=post.likes.get(userId);
+
+        if (isLiked){
+            post.likes.delete(userId);
+        }else{
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost=await Post.findByIdAndUpdate(
+            id,
+            {likes:post.likes},
+            {new:true},
+
+        ); 
+        res.status(200).json();
+
+
+    }catch(err){
+         res.status(404).json({message: err.message})
 
     }
 }
