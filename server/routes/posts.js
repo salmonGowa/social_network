e5a45f5f349de56dd99d51619d0f8fc51ea04632
Salmon Gowa <67318860+salmonGowa@users.noreply.github.com>
2023-07-27 11:39:58 +0300

    authentication

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,3 +1,4 @@
+/*
 import express from "express";
 import {getFeedPosts, getUserPosts, likePost} from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
@@ -6,10 +7,24 @@ const router=express.Router();
 
 /**Read */
 
-router.get("/", verifyToken,getFeedPosts);
-router.get("/userId/posts", verifyToken, getUserPosts);
+//router.get("/", verifyToken,getFeedPosts);
+//router.get("/userId/posts", verifyToken, getUserPosts);
 /**update */
 
-router.patch("/:id/like",verifyToken,likePost);
+//router.patch("/:id/like",verifyToken,likePost);
+
+//export default router;
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
